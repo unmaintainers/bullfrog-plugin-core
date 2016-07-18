14d8b1e70dac014a522aa8dbed36bc692e2fbfbd
roylaurie <roy.laurie@asimovian.software>
2016-07-18 11:42:24 -0700

    wip

---
+++ README.md
@@ -0,0 +1,6 @@
+Bullfrog Voter
+----------------------------------------
+
+**Voting Plugin for Bullfrog STEEM Service**
+
+Automatically up-votes on configured conditions.
---
+++ src/bullfrog/plugin/voter/Voter.js
@@ -0,0 +1,25 @@
+define(
+	'bullfrog/service/APlugin'
+), (
+	APlugin
+) {
+
+function VoterPlugin = function(bullfrogService) {
+	this._service = bullfrogService;
+};
+
+VoterPlugin.prototype = Object.create(APlugin.prototype);
+
+VoterPlugin.prototype.init = function() {
+	this.getBlockEmitter.on(function(block) {
+	});
+};
+
+VoterPlugin.prototype.start = function() {
+};
+
+VoterPlugin.prototype.stop = function() {
+};
+
+return VoterPlugin;
+)};
